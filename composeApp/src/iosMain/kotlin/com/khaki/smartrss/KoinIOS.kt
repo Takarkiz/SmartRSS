4a82e83c1e4604ef46d8ec895329e8dbb175b8d6
Takarkiz <ponpontanuki16@gmail.com>
2025-09-15 17:58:01 +0900

    iOS側でのKoinの初期化処理を追加

---
+++ composeApp/src/iosMain/kotlin/com/khaki/smartrss/KoinIOS.kt
@@ -0,0 +1,5 @@
+package com.khaki.smartrss
+
+fun doInitKoin() {
+    initKoin()
+}
--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,7 +1,13 @@
 import SwiftUI
+import ComposeApp
 
 @main
 struct iOSApp: App {
+
+    init() {
+        KoinIOSKt.doInitKoin()
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
