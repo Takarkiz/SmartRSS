c885d0e3b1f340d68c5287319d7bc6387261db81
Takarkiz <ponpontanuki16@gmail.com>
2025-10-06 16:21:09 +0900

    フルパス指定を修正

--- modules/Room/src/commonMain/kotlin/com/khaki/room/AppDatabase.kt
+++ modules/Room/src/commonMain/kotlin/com/khaki/room/AppDatabase.kt
@@ -5,12 +5,13 @@ import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.khaki.room.dao.RssCategoryDao
 import com.khaki.room.dao.RssFeedDao
+import com.khaki.room.entity.ListStringTypeConverter
 import com.khaki.room.entity.RSSGroupTypeConverter
 import com.khaki.room.entity.RssCategoryEntity
 import com.khaki.room.entity.RssFeedEntity
 
 @Database(entities = [RssCategoryEntity::class, RssFeedEntity::class], version = 1)
-@TypeConverters(RSSGroupTypeConverter::class, com.khaki.room.entity.ListStringTypeConverter::class)
+@TypeConverters(RSSGroupTypeConverter::class, ListStringTypeConverter::class)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun rssCategoryDao(): RssCategoryDao
 
