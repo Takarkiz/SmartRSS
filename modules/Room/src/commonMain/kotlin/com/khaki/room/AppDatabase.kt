388dbf9f188636c44f00c603858249d9e7cecada
Takarkiz <ponpontanuki16@gmail.com>
2025-12-13 23:03:14 +0900

    iOSでビルドができるようDatabaseをプラットフォームごとに作成する
    https://github.com/Takarkiz/SmartRSS/issues/16

--- modules/Room/src/commonMain/kotlin/com/khaki/room/AppDatabase.kt
+++ modules/Room/src/commonMain/kotlin/com/khaki/room/AppDatabase.kt
@@ -1,8 +1,11 @@
 package com.khaki.room
 
+import androidx.room.ConstructedBy
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.RoomDatabaseConstructor
 import androidx.room.TypeConverters
+import androidx.sqlite.driver.bundled.BundledSQLiteDriver
 import com.khaki.room.dao.RssCategoryDao
 import com.khaki.room.dao.RssFeedDao
 import com.khaki.room.entity.ListStringTypeConverter
@@ -11,14 +14,31 @@ import com.khaki.room.entity.RssCategoryEntity
 import com.khaki.room.entity.RssCategoryGroupDetailConverter
 import com.khaki.room.entity.RssFeedEntity
 
-@Database(entities = [RssCategoryEntity::class, RssFeedEntity::class], version = 1)
+@Database(
+    entities = [RssCategoryEntity::class, RssFeedEntity::class],
+    version = 1,
+)
 @TypeConverters(
     RSSGroupTypeConverter::class,
     ListStringTypeConverter::class,
     RssCategoryGroupDetailConverter::class
 )
+@ConstructedBy(AppDatabaseConstructor::class)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun rssCategoryDao(): RssCategoryDao
 
     abstract fun rssFeedDao(): RssFeedDao
+}
+
+@Suppress("KotlinNoActualForExpect")
+expect object AppDatabaseConstructor : RoomDatabaseConstructor<AppDatabase> {
+    override fun initialize(): AppDatabase
+}
+
+fun getAppDatabase(
+    builder: RoomDatabase.Builder<AppDatabase>
+): AppDatabase {
+    return builder
+        .setDriver(BundledSQLiteDriver())
+        .build()
 }
