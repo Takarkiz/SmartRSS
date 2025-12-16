34b54f5e7ad3c7809fc0dbd4debb4c5c4bef700e
Takarkiz <ponpontanuki16@gmail.com>
2025-12-16 21:57:58 +0900

    外部ブラウザへの遷移が失敗していたので修正をした
    https://github.com/Takarkiz/SmartRSS/issues/38

--- composeApp/src/iosMain/kotlin/com/khaki/smartrss/platform/ExternalBrowserLauncher.ios.kt
+++ composeApp/src/iosMain/kotlin/com/khaki/smartrss/platform/ExternalBrowserLauncher.ios.kt
@@ -2,9 +2,11 @@ package com.khaki.smartrss.platform
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import kotlinx.cinterop.ExperimentalForeignApi
 import platform.Foundation.NSURL
 import platform.UIKit.UIApplication
 
+@OptIn(ExperimentalForeignApi::class)
 @Composable
 actual fun ExternalBrowserLauncher(
     url: String?,
@@ -15,7 +17,7 @@ actual fun ExternalBrowserLauncher(
 
         val nsUrl = NSURL(string = url)
         if (UIApplication.sharedApplication.canOpenURL(nsUrl)) {
-            UIApplication.sharedApplication.openURL(nsUrl)
+            UIApplication.sharedApplication.openURL(nsUrl, options = emptyMap<Any?, Any>(), completionHandler = null)
         }
         onLaunched()
     }
